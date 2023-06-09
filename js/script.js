6d67048789103ad952b8cc28361d471c9084a2a8
aalexebr <alex.ebr@icloud.com>
2023-06-09 14:37:56 +0200

    added an addition

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ let first_name = prompt ("enter name");
 let last_name = prompt ("enter surname");
 let color = prompt ("enter favorite color");
 
-const result = document.getElementById('ResultPrompt');
+let result = document.getElementById('ResultPrompt');
 result.innerHTML = first_name + ' ' + last_name + ' ' + color;
 
 console.log(result.innerHTML)
+
+let x = prompt ("enter first number")
+let y = prompt ("enter second number")
+let num1 = parseInt(x);
+let num2 = parseInt(y);
+
+let result2 = document.getElementById('ResultPrompt2');
+result2.innerHTML = num1 + num2;
